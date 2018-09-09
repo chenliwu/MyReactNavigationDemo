9d447c562d0c0a680070b5ad1633b021cb8ff1c7
chenliwu <30075213@qq.com>
2018-09-09 22:13:36 +0800

    SwitchNavigator

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import MyStackNavigator from './src/stackNavigator/MyStackNavigator';
+import MySwitchNavigator from './src/switchNavigator/MySwitchNavigator';
 
-AppRegistry.registerComponent('MyReactNavigationDemo', () => MyStackNavigator);
+AppRegistry.registerComponent('MyReactNavigationDemo', () => MySwitchNavigator);
---
+++ src/switchNavigator/MySwitchNavigator.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { 
+    View,
+    Text
+ } from 'react-native';
+
+import {
+    createSwitchNavigator
+} from 'react-navigation';
+
+
+import LanuchPage from './../pages/LanuchPage';
+import MainPage from './../pages/MainPage';
+import LoginPage from './../pages/LoginPage';
+
+/**
+ * SwitchNavigator 的用途是一次只显示一个页面。 默认情况下，它不处理返回操作，并在你切换时将路由重置为默认状态。
+ */
+const MySwitchNavigator  = createSwitchNavigator(
+    {
+        LanuchPage:{
+            screen:LanuchPage,
+        },
+        MainPage:MainPage,
+        LoginPage:LoginPage,
+    },
+    {
+        initialRouteName:"LanuchPage",
+    }
+);
+
+export default class MySwitchNavigatorPage extends React.Component{
+
+    render(){
+        return (
+            <MySwitchNavigator/>
+        );
+    }
+
+}
+
+
