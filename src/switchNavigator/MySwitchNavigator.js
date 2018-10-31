20f02eef4646081c1a3fef67f4154d4c9de67327
chenlw <>
2018-10-31 08:40:03 +0800

    练习

--- src/switchNavigator/MySwitchNavigator.js
+++ src/switchNavigator/MySwitchNavigator.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { 
+import {
     View,
     Text
- } from 'react-native';
+} from 'react-native';
 
 import {
     createSwitchNavigator
@@ -16,22 +16,22 @@ import LoginPage from './../pages/LoginPage';
 /**
  * SwitchNavigator 的用途是一次只显示一个页面。 默认情况下，它不处理返回操作，并在你切换时将路由重置为默认状态。
  */
-const MySwitchNavigator  = createSwitchNavigator(
+const MySwitchNavigator = createSwitchNavigator(
     {
-        LanuchPage:{
-            screen:LanuchPage,
+        LanuchPage: {
+            screen: LanuchPage,
         },
-        MainPage:MainPage,
-        LoginPage:LoginPage,
+        MainPage: MainPage,
+        LoginPage: LoginPage,
     },
     {
-        initialRouteName:"LanuchPage",
+        initialRouteName: "LanuchPage",
     }
 );
 
-export default class MySwitchNavigatorPage extends React.Component{
+export default class MySwitchNavigatorPage extends React.Component {
 
-    render(){
+    render() {
         return (
             <MySwitchNavigator/>
         );
