ce8c35a51e5dfbeca45d5d61398545544a7ffc25
chenlw <>
2018-10-31 17:35:04 +0800

    测试

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import MyStackNavigator from './src/stackNavigator/MyStackNavigator';
-import MySwitchNavigator from './src/switchNavigator/MySwitchNavigator';
+//import MySwitchNavigator from './src/switchNavigator/MySwitchNavigator';
 
 import StackNavigatorBasics from './src/OldReactNavigation/StackNavigator/StackNavigatorBasics';
 
+/**
+ * 测试
+ */
 AppRegistry.registerComponent('MyReactNavigationDemo', () => StackNavigatorBasics);
